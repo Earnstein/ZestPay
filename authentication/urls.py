70ea39ccfd1ff8e82dc8ad5e29f389410e6784df
Bakare Damilola <bdamilola00@gmail.com>
2024-01-19 22:36:01 +0100

    added login and logout urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,12 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 app_name = "auths"
 
 
 urlpatterns = [
-    path("register/", views.httpRegisterUser, name="index")
+    path("register/", views.httpRegisterUser, name="index"),
+    path("login/", views.httpLogin, name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
